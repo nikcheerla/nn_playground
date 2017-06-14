7c5ab313a0249ad060399c72d454c2a00bed1408
Nikhil Cheerla <nikcheerla@gmail.com>
2017-06-14 16:00:02 -0700

    hn more update

--- hypernetworks/conv_hypernet_mnist.py
+++ hypernetworks/conv_hypernet_mnist.py
@@ -39,48 +39,54 @@ print ('Y_train shape:', Y_train.shape)
 print (X_train.shape[0], 'train samples')
 print (X_test.shape[0], 'test samples')
 
-X_train, X_test = torch.Tensor(X_train).float().cuda(), torch.Tensor(X_test).float().cuda()
-Y_train, Y_test = torch.Tensor(Y_train).long().cuda(), torch.Tensor(Y_test).long().cuda()
+X_train, X_test = torch.Tensor(X_train).float(), torch.Tensor(X_test).float()
+Y_train, Y_test = torch.Tensor(Y_train).long(), torch.Tensor(Y_test).long()
 
 
-def one_hot_encode_tensor(layer_num=0, dilation_val=1, num_layers=6, max_dilation=3):
+
+NUM_LAYERS = 6
+MAX_DILATION = 3
+FILTERS = 32
+KERNEL_SIZE = 3
+DROPOUT_RATE = 0.3
+
+
+def one_hot_encode_tensor(layer_num=0, dilation_val=1, num_layers=NUM_LAYERS, max_dilation=MAX_DILATION):
     #Constant variable array
-    np.zeros((1, num_layers+max_dilation))
+    array = np.zeros((1, num_layers+max_dilation))
     array[0, layer_num] = 1
     array[0, (num_layers - 1):(num_layers + dilation_val - 1)] = 1
     array = Variable(torch.Tensor(array).float(), requires_grad=False)
 
     return array
 
-filters = 32
-kernel_size = (3, 3)
-dropout_rate = 0.3
-
 class HyperNet(nn.Module):
     def __init__(self):
         super(HyperNet, self).__init__()
 
-        self.fc1 = nn.Linear(4, 32)
+        self.fc1 = nn.Linear(NUM_LAYERS + MAX_DILATION, FILTERS*FILTERS//32)
         self.conv1 = nn.Conv2d(2, 4, kernel_size=(3, 3), padding=(1, 1))
         self.conv2 = nn.Conv2d(4, 6, kernel_size=(3, 3), padding=(1, 1))
         self.conv3 = nn.Conv2d(6, 8, kernel_size=(3, 3), padding=(1, 1))
-        self.conv4 = nn.Conv2d(8, 9, kernel_size=(3, 3), padding=(1, 1))
+        self.conv4 = nn.Conv2d(8, KERNEL_SIZE*KERNEL_SIZE, kernel_size=(3, 3), padding=(1, 1))
         
     def forward(self, x):
-        x = x.cuda()
+
+        if torch.cuda.is_available():
+            x = x.cuda()
+
         x = F.tanh(self.fc1(x))
-        x = x.view(1, 2, 4, 4)
-        x = F.dropout(F.relu(self.conv1(x)), p=0.3, training=self.training)
-        #print (x.size())
+        x = x.view(1, 2, FILTERS/8, FILTERS/8)
+        x = F.dropout(F.relu(self.conv1(x)), p=DROPOUT_RATE, training=self.training)
         x = F.upsample_bilinear(x, scale_factor=2)
-        x = F.dropout(F.relu(self.conv2(x)), p=0.3, training=self.training)
+        x = F.dropout(F.relu(self.conv2(x)), p=DROPOUT_RATE, training=self.training)
         x = F.upsample_bilinear(x, scale_factor=2)
-        x = F.dropout(F.relu(self.conv3(x)), p=0.3, training=self.training)
+        x = F.dropout(F.relu(self.conv3(x)), p=DROPOUT_RATE, training=self.training)
         x = F.upsample_bilinear(x, scale_factor=2)
         x = F.tanh(self.conv4(x))
         #print (x.size())
-        x = x.view(16, 3, 16, )
-        x = x.permute(0, 2, 1, 3) * 0.1
+        x = x.view(FILTERS, FILTERS, KERNEL_SIZE, KERNEL_SIZE)
+        x = x.permute(0, 1, 2, 3) * 0.1
         #print ("Weight1 mean: ", self.fc1._parameters['weight'].mean().data.numpy())
         #print ("Weight2 mean: ", self.fc2._parameters['weight'].mean().data.numpy())
         #print ("Activation mean: ", x.mean().data.numpy())
@@ -90,39 +96,53 @@ class HyperNet(nn.Module):
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.hypernet = HyperNet().cuda()
-        self.input = nn.Conv2d(1, 32, kernel_size=(1, 1))
+        self.hypernet = HyperNet()
 
-        self.conv1 = nn.Conv2d(32, 32, kernel_size=(3, 3), padding=(1, 1))
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=(3, 3), padding=(1, 1))
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=(3, 3), padding=(1, 1))
-        self.conv4 = nn.Conv2d(32, 32, kernel_size=(3, 3), padding=(1, 1))
+        self.input = nn.Conv2d(1, FILTERS, kernel_size=(1, 1))
+
+        self.conv1 = nn.Conv2d(FILTERS, FILTERS, kernel_size=KERNEL_SIZE, padding=(1, 1))
+        self.conv2 = nn.Conv2d(FILTERS, FILTERS, kernel_size=KERNEL_SIZE, padding=(1, 1))
+        self.conv3 = nn.Conv2d(FILTERS, FILTERS, kernel_size=KERNEL_SIZE, padding=(1, 1))
+        self.conv4 = nn.Conv2d(FILTERS, FILTERS, kernel_size=KERNEL_SIZE, padding=(1, 1))
         self.fc = nn.Linear(32, 10)
+
+        if torch.cuda.is_available():
+            self.hypernet.cuda()
+            self.cuda()
         
     def forward(self, x):
-        x = x.cuda()
+
+        if torch.cuda.is_available():
+            x = x.cuda()
+
         x = self.input(x)
 
         self.hypernet.training = self.training
-        conv1_weights = self.hypernet(one_hot_encode_tensor(0))
-        conv2_weights = self.hypernet(one_hot_encode_tensor(1))
-        conv3_weights = self.hypernet(one_hot_encode_tensor(2))
-        conv4_weights = self.hypernet(one_hot_encode_tensor(3))
+        conv1_weights = self.hypernet(one_hot_encode_tensor(layer_num=0, dilation_val=1))
+        conv2_weights = self.hypernet(one_hot_encode_tensor(layer_num=1, dilation_val=1))
+        conv3_weights = self.hypernet(one_hot_encode_tensor(layer_num=2, dilation_val=2))
+        conv4_weights = self.hypernet(one_hot_encode_tensor(layer_num=3, dilation_val=2))
+        conv5_weights = self.hypernet(one_hot_encode_tensor(layer_num=4, dilation_val=3))
+        conv6_weights = self.hypernet(one_hot_encode_tensor(layer_num=5, dilation_val=3))
 
-        x = F.relu(F.conv2d(x, conv1_weights, bias=self.conv1._parameters['bias'], padding=1))
-        x = F.relu(F.conv2d(x, conv2_weights, bias=self.conv2._parameters['bias'], padding=1))
+        x = F.relu(F.conv2d(x, conv1_weights, bias=self.conv1._parameters['bias'], padding=1, dilation=1))
+        x = F.relu(F.conv2d(x, conv2_weights, bias=self.conv2._parameters['bias'], padding=1, dilation=1))
         x = F.dropout(x, p=0.3, training=self.training)
-        x = F.max_pool2d(x, 2)
-        x = F.relu(F.conv2d(x, conv3_weights, bias=self.conv3._parameters['bias'], padding=1))
-        x = F.relu(F.conv2d(x, conv4_weights, bias=self.conv4._parameters['bias'], padding=1))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(F.conv2d(x, conv4_weights, bias=None, padding=1))
+        x = F.max_pool2d(x, 2, stride=1)
+
+        x = F.relu(F.conv2d(x, conv3_weights, bias=self.conv3._parameters['bias'], padding=1, dilation=2))
+        x = F.relu(F.conv2d(x, conv4_weights, bias=self.conv4._parameters['bias'], padding=1, dilation=2))
+        x = F.max_pool2d(x, 2, stride=1)
+        x = F.relu(F.conv2d(x, conv5_weights, bias=None, padding=1, dilation=4))
+        x = F.relu(F.conv2d(x, conv6_weights, bias=None, padding=1, dilation=4))
+        x = F.max_pool2d(x, x.size(2), stride=1)
+
         x = x.view(x.size(0), -1)
         x = F.dropout(x, p=0.4, training=self.training)
         x = self.fc(x)
         return x
 
-net = Net().cuda()
+net = Net()
 print(net)
 
 model = ModuleTrainer(net)
@@ -139,6 +159,6 @@ model.compile(loss=F.cross_entropy,
 
 model.summary([1, 28, 28])
 
-model.fit(X_train, Y_train, batch_size=128, nb_epoch=5, verbose=1, val_data=(X_test, Y_test), cuda_device=0)
+model.fit(X_train, Y_train, batch_size=128, nb_epoch=5, verbose=1, val_data=(X_test, Y_test), cuda_device=-1)
 
 #IPython.embed()
